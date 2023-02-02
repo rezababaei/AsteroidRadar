986e1eb8ebeae76e67995edc30a2ac52a656bfb0
Reza Babaei <rzbabaei1@gmail.com>
2023-02-02 12:27:44 +0100

    theming Added dimension added rotation added supporting different languages added

--- app/src/main/java/com/udacity/asteroidradar/util/BindingAdapters.kt
+++ app/src/main/java/com/udacity/asteroidradar/util/BindingAdapters.kt
@@ -3,6 +3,8 @@ package com.udacity.asteroidradar.util
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
@@ -14,16 +16,24 @@ import com.udacity.asteroidradar.util.Constants.IMAGE_TYPE
 
 @BindingAdapter("setPictureOfDayAndDescription")
 fun bindPictureOfDayAndDescription(imageView: ImageView, pictureOfDay: PictureOfDay?) {
-    pictureOfDay?.let { picture ->
-        imageView.contentDescription = picture.title
-        if (picture.mediaType == IMAGE_TYPE) {
+    val context = imageView.context
+    if (pictureOfDay != null) {
+        imageView.contentDescription =
+            String.format(context.getString(R.string.nasa_picture_of_day_content_description_format),
+                pictureOfDay.title)
+
+        if (pictureOfDay.mediaType == IMAGE_TYPE) {
             Picasso.get()
-                .load(picture.url)
+                .load(pictureOfDay.url)
                 .placeholder(R.drawable.placeholder_picture_of_day)
                 .error(R.drawable.ic_launcher_foreground)
                 .into(imageView)
         }
+    } else {
+        imageView.contentDescription =
+            context.getString(R.string.this_is_nasa_s_picture_of_day_showing_nothing_yet)
     }
+
 }
 
 /**
@@ -41,8 +51,12 @@ fun bindRecyclerView(recyclerView: RecyclerView, data: List<Asteroid>?) {
 
 @BindingAdapter("statusIcon")
 fun bindAsteroidStatusImage(imageView: ImageView, isHazardous: Boolean) {
+    val context = imageView.context
+
     if (isHazardous) {
         imageView.setImageResource(R.drawable.ic_status_potentially_hazardous)
+        DrawableCompat.setTint(imageView.drawable,
+            ContextCompat.getColor(context, R.color.potentially_hazardous))
     } else {
         imageView.setImageResource(R.drawable.ic_status_normal)
     }
@@ -52,10 +66,12 @@ fun bindAsteroidStatusImage(imageView: ImageView, isHazardous: Boolean) {
 fun bindDetailsStatusImage(imageView: ImageView, isHazardous: Boolean) {
     if (isHazardous) {
         imageView.setImageResource(R.drawable.asteroid_hazardous)
-        imageView.contentDescription=imageView.resources.getString(R.string.potentially_hazardous_asteroid_image)
+        imageView.contentDescription =
+            imageView.resources.getString(R.string.potentially_hazardous_asteroid_image)
     } else {
         imageView.setImageResource(R.drawable.asteroid_safe)
-        imageView.contentDescription=imageView.resources.getString(R.string.not_hazardous_asteroid_image)
+        imageView.contentDescription =
+            imageView.resources.getString(R.string.not_hazardous_asteroid_image)
 
     }
 }
@@ -85,3 +101,5 @@ fun bindTextViewToDisplayVelocity(textView: TextView, number: Double) {
 fun goneIfNotNullOrEmpty(view: View, asteroids: List<Asteroid>?) {
     view.visibility = if (asteroids.isNullOrEmpty()) View.VISIBLE else View.GONE
 }
+
+
